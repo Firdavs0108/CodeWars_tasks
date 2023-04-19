72b088583ab1fab97885f3a7bd547b8f5377141a
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-20 01:10:24 +0900

    Practice

---
+++ Practice/practice.js
@@ -0,0 +1,48 @@
+
+
+///Find even and odd numbers
+
+// let key = 11
+
+// if(key % 2 == 0 ){
+//     console.log('Even number');
+// } else{
+//     console.log('Odd number');
+// }
+
+
+
+//// Create score table
+
+// let score = 33
+
+// if(score >= 100  || score >=95){
+//     console.log('A+');
+// }else if(score >=94 || score>=86){
+//     console.log('A');
+// }else if(score >= 85 || score >= 60){
+//     console.log('B');
+// }else if(score >= 59 || score >= 45){
+//     console.log('C');
+// }else{
+//     console.log('FAILED');
+// }
+
+
+
+
+
+/// WORKING WITH SWITCH CASE
+
+
+var fruit = 'Apple'
+
+switch ('Apple') {
+    case 'Apple':
+        console.log('10.000')
+    case 'Peach' :
+        console.log('12.000');
+    case 'PineApple':
+        console.log('15.000');
+
+}
