e6ab1eaa3a357d3bcf557fc399f925dbe2d61392
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-20 10:44:41 +0900

    basicOp with switch case

---
+++ 20.09.23/task2.js
@@ -0,0 +1,18 @@
+
+
+
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+        case '+':
+            console.log(value1 + value2);
+        case '-':
+                console.log(value1 + value2); 
+        case '*':
+                    console.log(value1 + value2); 
+                
+            
+    
+        default:
+            break;
+    }
+}
