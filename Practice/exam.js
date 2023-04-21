410211ab1f68a6fda8fe7bbbcdf0cd26e8c490c1
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-21 23:46:07 +0900

    exam.tasks

---
+++ Practice/exam.js
@@ -0,0 +1,47 @@
+
+
+// console.log('backend' && false || 'frontend ' && 'mobile');
+// console.log(isFinite(true) && isNaN(false)  ||  'webbrain');
+// console.log((false ?? false) && true);
+// console.log(!(false || false) && true);
+
+
+
+///Update Object
+
+// let list = [
+//     {id:1, name:'firdavs', title: 'CEO'},
+//     {id:2, name:'ABU', title: 'CTO'},
+//     {id:3, name:'JABO', title: 'HR'},
+//     {id:4, name:'MAGA', title: 'CEB'}
+// ]
+
+// const onUpdate = (ids) => {
+//     keys = list.map(value => (value.id == ids.id) ? {...value, ...ids} : value)
+//     list = keys
+    
+// }
+// onUpdate({id:4,title:'DRT'})
+// console.log(keys);
+
+
+// let obj = {
+//     name: 'Webbrain',
+//     title: 'IT Center'
+// }
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+// // console.log(Object.fromEntries(obj));
+
+
+//////////
+
+const moment = (locale) => {
+
+    console.log(locale);
+}
+
+ moment('webbrain')
+
