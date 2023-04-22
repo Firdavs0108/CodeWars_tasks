c3001c3cc003ba012184dab21d3377adcb46dc02
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-22 23:32:02 +0900

    Codewars

---
+++ Codewars/task4.js
@@ -0,0 +1,15 @@
+
+
+///Add string numbers
+
+function sumStr(a,b) {
+    let num = Number(a)
+    let num2 = Number(b)
+
+    let key = num + num2
+    
+   console.log(key);
+
+}
+
+sumStr('20','30')
