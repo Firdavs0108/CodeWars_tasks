a9ed9c19d8ec7c66da386372b7cd9153ecdecb7c
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-04-24 21:03:14 +0900

    Compare array

---
+++ exam/task14.js
@@ -0,0 +1,43 @@
+
+
+/////14
+
+// let array = [11, 10, 1];
+// let result = true;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 100) {
+//      result = false;
+//   }
+// }
+// console.log(result)
+
+
+// let array = [11, 10, 22];
+// let result = true;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 100) {
+//     result = false;
+//   }
+// }
+
+// console.log(result);
+
+
+
+// function Numberarray(array){
+    
+//     let result = true;
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 100) {
+//     result = false;
+//     break;
+//   }
+//   console.log(result);
+// }
+
+// }
+// // console.log(Numberarray[10,20,20,50]);
+// Numberarray([10,20,30])
